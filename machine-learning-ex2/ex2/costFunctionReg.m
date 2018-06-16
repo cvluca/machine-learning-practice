66536f5ee39d741c86a546d084dbd77c46516dce
Luca(Wei) Chen <z5155740@unsw.edu.au>
2018-06-17 03:17:27 +1000

    finish ex2

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,7 +17,10 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
+h = sigmoid(X*theta);
+J = (1/m)*(-y'*log(h) - (1-y)'*log(1-h)) + (lambda/(2*m))*sum(theta(2:end,1).^2);
+grad = (1/m)*X'*(h-y);
+grad(2:end,1) = grad(2:end,1) + (lambda/m)*theta(2:end,1);
 
 
 
