f780f5eb78116f681cac92c2183d88f26359b076
cvluca <z5155740@unsw.edu.au>
2018-07-02 02:34:11 +0800

    finish ex4

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,10 +62,11 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-z2 = [ones(m, 1) X] * Theta1';
-a2 = sigmoid(z2);
-z3 = [ones(size(a2, 1), 1) a2] * Theta2';
-h = sigmoid(z3);
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
+z3 = a2 * Theta2';
+a3 = h = sigmoid(z3);
 
 Y = zeros(m, num_labels);
 for i=1:m
@@ -74,6 +75,18 @@ end
 
 J = (1/m) * sum(sum(-Y .* log(h) - (1 - Y) .* log(1 - h)));
 J = J + (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2))+sum(sum(Theta2(:,2:end).^2)));
+
+delta_3 = a3 - Y;
+delta_2 = (delta_3 * Theta2 .* sigmoidGradient([ones(size(z2, 1), 1) z2]))(:,2:end);
+
+delta_1 = delta_2' * a1;
+delta_2 = delta_3' * a2;
+
+Theta1(:,1) = zeros(size(Theta1), 1);
+Theta2(:,1) = zeros(size(Theta2), 1);
+
+Theta1_grad = delta_1./m + (lambda/m)*Theta1;
+Theta2_grad = delta_2./m + (lambda/m)*Theta2;
 
 % -------------------------------------------------------------
 
